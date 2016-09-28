1927c2575f72ea0c17f8928da54debf655a306aa
Kate Shaffer <kate.shaffer@gmail.com>
2016-09-28 15:26:36 -0700

    Created initial tasks routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'tasks#index'
+  get 'tasks' => 'tasks#index'
+  get 'tasks/:identifier' => 'tasks#show'
+  # get 'tasks/new' => 'tasks#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
