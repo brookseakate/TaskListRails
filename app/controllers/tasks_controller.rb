f709836d4f778d0d3cacba0b4d0ebcb2a5f50a50
Kate Shaffer <kate.shaffer@gmail.com>
2016-09-28 15:39:52 -0700

    Updated column names to match new baseline description

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,47 +2,47 @@ class TasksController < ApplicationController
 
   def index
     @tasks = {
-      buy_gummy_bears: { name: "buy gummy bears",
+      buy_gummy_bears: { title: "buy gummy bears",
         description: "need gummy bears from duane reade for lunch",
         completion_status: "on deck",
-        completion_date: nil },
-      eat_gummy_bears: { name: "eat gummy bears",
+        completed_at: nil },
+      eat_gummy_bears: { title: "eat gummy bears",
         description: "need to eat the gummy bears at lunch; blocked until gummy bears have been purchased",
         completion_status: "blocked",
-        completion_date: nil },
-      buy_cheez_its: { name: "buy cheez-its",
+        completed_at: nil },
+      buy_cheez_its: { title: "buy cheez-its",
         description: "need to buy cheez-its from brightly-lit bodega",
         completion_status: "complete",
-        completion_date: "2016-09-26" },
-      eat_cheez_its: { name: "eat cheez-its",
+        completed_at: "2016-09-26" },
+      eat_cheez_its: { title: "eat cheez-its",
         description: "need to eat cheez-its all the time",
         completion_status: "in progress",
-        completion_date: nil },
+        completed_at: nil },
     }
   end #index
 
   def show
     tasks = {
-      buy_gummy_bears: { name: "buy gummy bears",
+      buy_gummy_bears: { title: "buy gummy bears",
         description: "need gummy bears from duane reade for lunch",
         completion_status: "on deck",
-        completion_date: nil },
-      eat_gummy_bears: { name: "eat gummy bears",
+        completed_at: nil },
+      eat_gummy_bears: { title: "eat gummy bears",
         description: "need to eat the gummy bears at lunch; blocked until gummy bears have been purchased",
         completion_status: "blocked",
-        completion_date: nil },
-      buy_cheez_its: { name: "buy cheez-its",
+        completed_at: nil },
+      buy_cheez_its: { title: "buy cheez-its",
         description: "need to buy cheez-its from brightly-lit bodega",
         completion_status: "complete",
-        completion_date: "2016-09-26" },
-      eat_cheez_its: { name: "eat cheez-its",
+        completed_at: "2016-09-26" },
+      eat_cheez_its: { title: "eat cheez-its",
         description: "need to eat cheez-its all the time",
-        completion_status: "in progress",
-        completion_date: nil },
+          completion_status: "in progress",
+        completed_at: nil },
     }
 
     puts "This is the identifier: #{params[:identifier]}"
-    # :identifier should be passed in as the name, referring here to the hash
+    # :identifier should be passed in as the title, referring here to the hash
     puts tasks[params[:identifier].to_sym]
 
     @task = tasks[params[:identifier].to_sym]
