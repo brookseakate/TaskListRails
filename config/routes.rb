99b8d463508bf23dabf86790ba76b9ce50762c54
Kate Shaffer <kate.shaffer@gmail.com>
2016-10-05 09:24:37 -0700

    Updated routes.rb to use resources route helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   root 'tasks#index'
-  get 'tasks' => 'tasks#index'
-  get 'tasks/new' => 'tasks#new'
-  get 'tasks/:id' => 'tasks#show', as: 'task'
-  post 'tasks' => 'tasks#create'
-  get 'tasks/:id/edit' => 'tasks#edit', as: 'task_edit'
-  patch 'tasks/:id' => 'tasks#update'
-  delete 'tasks/:id' => 'tasks#destroy'
+
+  resources :tasks
 
   patch 'tasks/:id/complete' => 'tasks#complete', as: 'task_complete'
 
