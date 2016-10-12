8490773388d3d88ba599f03bcc2e9064563102b9
Kate Shaffer <kate.shaffer@gmail.com>
2016-10-11 19:20:01 -0700

    Built seed data, used Faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+4.times do |i|
+  Task.create(title: "#{Faker::Hacker.verb.capitalize} #{Faker::Superhero.power} Book",
+  description: "#{Faker::Beer.hop} #{Faker::StarWars.quote}",
+  completed_at: Faker::Time.backward(30),
+  completion_status: ["completed", "accepted"].sample)
+end
+
+4.times do |i|
+  Task.create(title: "#{Faker::Hacker.verb.capitalize} #{Faker::Superhero.power} Book",
+  description: "#{Faker::Beer.hop} #{Faker::StarWars.quote}",
+  completion_status: ["defined", "deep freeze", "on deck", "in progress", "blocked"].sample)
+end
